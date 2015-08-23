228f1bd64e587829221e8b7d55a1b6fb1c231258
kenblizzard <kenneth.bolico@gmail.com>
2015-08-23 21:19:53 +0800

    add new model, seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use App\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,8 +15,26 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-        // $this->call(UserTableSeeder::class);
+        $this->call('UserTableSeeder');
+
+        $this->command->info('User table seeded!');
 
         Model::reguard();
+    }
+}
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       DB::table('users')->delete();
+
+        User::create(array('name' => 'foo@bar.com'));
+        User::create(array('name' => 'test'));
     }
 }
---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use App\User
+
+// class UserTableSeeder extends Seeder
+// {
+//     /**
+//      * Run the database seeds.
+//      *
+//      * @return void
+//      */
+//     public function run()
+//     {
+//        DB::table('users')->delete();
+
+//         User::create(array('email' => 'foo@bar.com',));
+//     }
+// }
